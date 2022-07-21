990357357bab137b91c107a565f91dc5f8a37d52
KeenFinance <keen.finance@proton.me>
2022-07-22 00:11:13 +0800

    jsx change to tsx
    jsx change to tsx

--- src/utils/Dropdown.jsx
+++ src/utils/Dropdown.jsx
@@ -24,8 +24,8 @@ function Dropdown({
         onClick={(e) => e.preventDefault()}
       >
         <span className='hidden md:flex'>{title}</span>
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 flex md:hidden " fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 flex md:hidden " fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3 5h12M9 3v2m1.048 9.5A18.022 18.022 0 016.412 9m6.088 9h7M11 21l5-10 5 10M12.751 5C11.783 10.77 8.07 15.61 3 18.129" />
         </svg>
         
         <svg className="w-3 h-3 fill-current text-gray-500 cursor-pointer ml-1 shrink-0 hidden md:flex" viewBox="0 0 12 12" xmlns="http://www.w3.org/2000/svg">
